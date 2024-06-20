0d7923bb2cb7ae292e98e67b48ccf61da114a86d
Shashank Kumar <beingshashankk@gmail.com>
2024-06-20 19:56:28 +0530

    adding some lined

--- day0.java
+++ day0.java
@@ -2,7 +2,7 @@ package PrepDsA;
 
 public class day0 {
     public static void main(String[] args) {
-        System.out.println("hello DSA");
+        System.out.println("hello DSA lets go");
     }
     
 }

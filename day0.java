bf866fda305a4f629d453db5dc1be3fe967f602e
Shashank Kumar <beingshashankk@gmail.com>
2024-06-20 20:19:54 +0530

    added some info

--- day0.java
+++ day0.java
@@ -3,6 +3,7 @@ package PrepDsA;
 public class day0 {
     public static void main(String[] args) {
         System.out.println("hello DSA lets go");
+        System.out.println("we will come here after some time");
     }
     
 }

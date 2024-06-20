d74e16641a23537e48da29aea6cffe8132d4ed34
Shashank Kumar <beingshashankk@gmail.com>
2024-06-20 19:33:24 +0530

    nothing impo

--- LoopsDAY2.java
+++ LoopsDAY2.java
@@ -1,1 +1,9 @@
 
+package PrepDsA;
+
+public class LoopsDAY2 {
+    public static void main(String[] args) {
+        System.out.println("LOOP is going to start with all the basic to intermidiate question");
+    }
+}
+
